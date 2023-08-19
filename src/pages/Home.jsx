ff0d449cb26012929cb53dafb41c209af35c544d
kmseunh <tmdgus8779@gmail.com>
2023-08-19 22:09:26 +0900

    feat: Fetch and display random photos on the Home page
    Refactor the 'Home' component to fetch and display a collection of random photos from the Unsplash API
    This change involves integrating the 'useState' and 'useEffect' hooks to manage the photo data and perform the API request
    The fetched photos are passed to the 'PhotoList' component for rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PhotoList from '../components/PhotoList';
 
 const Home = () => {
-    return <h2>Welcome to Gallery.</h2>;
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        const Access_Key = 'WGLIaeuwXsRAO2p4Cqj5jP43s4G6tkacH_5kf8AQF24';
+
+        const fetchPhotos = async () => {
+            try {
+                const response = await axios.get(
+                    'https://api.unsplash.com/photos/random',
+                    {
+                        params: {
+                            client_id: Access_Key,
+                            count: 30,
+                        },
+                    }
+                );
+                setPhotos(response.data);
+            } catch (error) {
+                console.error('사진을 불러오는 중 오류 발생:', error);
+            }
+        };
+
+        fetchPhotos();
+    }, []);
+
+    return (
+        <div>
+            <PhotoList photos={photos} />
+        </div>
+    );
 };
 
 export default Home;
