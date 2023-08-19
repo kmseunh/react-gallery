4c0343b2df26d95a8343a85e9021166f788cef9b
kmseunh <tmdgus8779@gmail.com>
2023-08-19 22:13:20 +0900

    refactor: Update routing configuration and rendering setup
    Revise the code in 'index.js' to enhance routing and rendering setup
    Replace the deprecated 'react-dom/client' import with 'react-dom', and simplify the nesting of the 'RouterProvider' around the 'App' component within a 'React.StrictMode'
    This adjustment improves code readability and maintains compatibility with the latest 'react-dom' version

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
@@ -16,7 +15,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RouterProvider router={router}>
-            {' '}
             <App />
         </RouterProvider>
     </React.StrictMode>
