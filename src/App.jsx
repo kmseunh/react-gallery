1bc15d3c4992055b122020fd25b30b6c6864e1b4
kmseunh <tmdgus8779@gmail.com>
2023-08-22 01:45:58 +0900

    style: code formatting using Prettier for improved consistency and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 
 const App = () => {
     return (
         <>
-            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/photo/:id' element={<Detail />} />
             </Routes>
-            <Footer />
         </>
     );
 };
